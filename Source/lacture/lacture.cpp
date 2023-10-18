8d63f0f3fff393ccf1eba64ad870afbbf012092b
polli789 <polli789@naver.com>
2023-10-18 14:45:50 +0900

    unreal2

---
+++ Source/lacture/lacture.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "lacture.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, lacture, "lacture" );
